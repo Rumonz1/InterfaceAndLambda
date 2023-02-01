761fdb0e30bb0f635f59e445f74f48fb7033fec6
Rumonz <lone_assassin@mail.ru>
2023-02-01 18:15:02 +0300

    Задания 1-4 через лямбду

--- src/FunctionWork.java
+++ src/FunctionWork.java
@@ -2,12 +2,7 @@ import java.util.function.Function;
 
 public class FunctionWork {
     public static void main(String[] args) {
-        Function<Double , Long> function = new Function<Double, Long>() {
-            @Override
-            public Long apply(Double aDouble) {
-                return Math.round(aDouble);
-            }
-        };
+        Function<Double , Long> function = a -> Math.round(a);
         System.out.println(function.apply(4.678));
         System.out.println(function.apply(293.111111));
         System.out.println(function.apply(5.99));
--- src/SupplierWork.java
+++ src/SupplierWork.java
@@ -2,12 +2,7 @@ import java.util.function.Supplier;
 
 public class SupplierWork {
     public static void main(String[] args) {
-        Supplier<Integer> supplier = new Supplier<Integer>() {
-            @Override
-            public Integer get() {
-                return (int) (Math.random()* 100);
-            }
-        };
+        Supplier<Integer> supplier =() -> (int) (Math.random()* 100);
         System.out.println(supplier.get());
     }
 }
