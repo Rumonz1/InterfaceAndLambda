86c9981a247d7a1ce2471305a51ab8161dd7481d
Rumonz <lone_assassin@mail.ru>
2023-02-03 22:40:08 +0300

    5 задание

--- src/Task5.java
+++ src/Task5.java
@@ -4,12 +4,11 @@ import java.util.function.Predicate;
 
 public class Task5 {
     public static void main(String[] args) {
-        Integer i = 10;
-        Predicate<Object> condition = q -> i >= 0;
-        Function<Object, String> ifTrue = s -> "Положительное число";
-        Function<Object, String> ifFalse = s -> "Отрицательное число";
-        Function<Object, String> function = ternaryOperator(condition, ifTrue, ifFalse) ;
-        System.out.println(function.apply(0));
+        Predicate<Integer> condition = q -> q >= 0;
+        Function<Integer, String> ifTrue = s -> "Положительное число";
+        Function<Integer, String> ifFalse = s -> "Отрицательное число";
+        Function<Integer, String> function = ternaryOperator(condition, ifTrue, ifFalse) ;
+        System.out.println(function.apply(-1));
     }
     public static <T, U> Function<T, U> ternaryOperator(
             Predicate<? super T> condition,
